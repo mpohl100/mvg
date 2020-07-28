437fa478cfd1e6e98f4a7827b7cbc6390880445a
mpohl <mpohl100@googlemail.com>
2020-07-28 21:07:39 +0200

    Erster Versuch die Sbahnen aufzubereiten

---
+++ parse_sbahn.py
@@ -0,0 +1,14 @@
+f = open("SBahnhoefe.txt", "r")
+data = {}
+current_line = ""
+for i,x in enumerate(f):
+    if i % 2 == 0:
+        current_line = x.replace(" ", "").replace("\n","").replace("\t","")
+        data[current_line] = []
+    else:
+        xx = x.split(" - ")
+        for bahnhof in xx:
+            data[current_line].append(bahnhof.replace(" ","").replace("\n","").replace("\t",""))
+w = open("SBahn.txt", "w")
+import json
+w.write(json.dumps(data))
