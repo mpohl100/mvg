d25b04a404537e34c7aeb61fde04533059e1de01
mpohl <mpohl100@googlemail.com>
2020-07-28 20:56:14 +0200

    U-Bahnhöfe in JSON umgewandelt

---
+++ parse_ubahn.py
@@ -0,0 +1,15 @@
+f = open("UBahnhoefe.txt", "r")
+data = {}
+current_line = ""
+for x in f:
+    if not x.startswith(">>"):
+        current_line = x.replace(" ", "").replace("\n","").replace("\t","")
+        data[current_line] = []
+    else:
+        data[current_line].append(x.replace(">>","").replace(" ","").replace("\n","").replace("\t",""))
+w = open("UBahn.txt", "w")
+import json
+w.write(json.dumps(data))
+
+
+
