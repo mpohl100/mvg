202cb0915ca7289de31a2daec9c722a1ed202ced
mpohl <mpohl100@googlemail.com>
2020-07-29 19:02:52 +0200

    Münchener Netz erfolgreich in python eingelesen

--- mvg_simulation.h
+++ mvg_simulation.h
@@ -3,34 +3,39 @@
 #include <string>
 #include <vector>
 
-namespace mvg {
+namespace mvg
+{
 
-	class Station {
+	class Station
+	{
 	public:
 		Station(std::string name) : name_(name) {}
 		Station() = default;
-		Station(Station const&) = default;
-		Station& operator=(Station const&) = default;
-		Station(Station&&) = default;
-		Station& operator=(Station&&) = default;
-	private:	
+		Station(Station const &) = default;
+		Station &operator=(Station const &) = default;
+		Station(Station &&) = default;
+		Station &operator=(Station &&) = default;
+
+	private:
 		std::string name_;
 	};
 
-	bool operator<(Station const& l, Station const& r);
-	bool operator>(Station const& l, Station const& r);
-	bool operator<=(Station const& l, Station const& r);
-	bool operator>=(Station const& l, Station const& r);
-	bool operator==(Station const& l, Station const& r);
-	bool operator!=(Station const& l, Station const& r);
+	bool operator<(Station const &l, Station const &r);
+	bool operator>(Station const &l, Station const &r);
+	bool operator<=(Station const &l, Station const &r);
+	bool operator>=(Station const &l, Station const &r);
+	bool operator==(Station const &l, Station const &r);
+	bool operator!=(Station const &l, Station const &r);
 
-	struct Line {
+	struct Line
+	{
 		std::vector<Station> stations_;
 	};
 
-	struct Network {
-		std::vector<Station> findRoute(Station const& start, Station const& end);
+	struct Network
+	{
+		std::vector<Station> findRoute(Station const &start, Station const &end);
 		std::vector<Line> lines_;
 	};
 
-}
+} // namespace mvg
