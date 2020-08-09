7303e7ae230deb43f938fa7efce8a41a276e2010
mpohl100 <mpohl100@googlemail.com>
2020-08-09 10:26:51 +0200

    Typ Annotationen eingefügt.

--- mvg_sim.py
+++ mvg_sim.py
@@ -1,9 +1,11 @@
 import json
 import copy
 from collections import defaultdict
+from typing import List, Set, Dict, Tuple, Optional
 
-def read_network(city):
-    network_raw = {}
+def read_network(city: str):
+    #                 linename  purpose   list of stations
+    network_raw: Dict[str, Dict[str, List[str]]] = {}
     # read U-Bahn
     with open('data/' + city + '_UBahn.json', 'r') as infile:
         ubahn_data = json.load(infile)
@@ -15,14 +17,14 @@ def read_network(city):
             network_raw[lineName] = stations
     return network_raw
 
-def find_all_stations(network):
-    all_stations = []
+def find_all_stations(network: Dict[str, List[str]]):
+    all_stations: List[str] = []
     for _, stations in network.items():
         all_stations.extend(stations)
     return set(all_stations)
 
-def index_network_by_line(network):
-    lines_per_station = {}
+def index_network_by_line(network: Dict[str, List[str]]):
+    lines_per_station: Dict[str, List[str]] = {}
     for line, stations in network.items():
         for s in stations:
             if s in lines_per_station:
@@ -32,15 +34,15 @@ def index_network_by_line(network):
     return lines_per_station
 
 
-def find_possible_switch_stations(lines_per_station, line):
-    ret = []
+def find_possible_switch_stations(lines_per_station: Dict[str, List[str]], line: str):
+    ret: List[str] = []
     for station, lines in lines_per_station.items():
         if line in lines and len(lines) >= 2:
             ret.append(station)
     return ret
 
-def find_all_switches(network, lines_per_station):
-    ret = {}
+def find_all_switches(network: Dict[str, List[str]], lines_per_station: Dict[str, List[str]]):
+    ret: Dict[str, List[str]] = {}
     for line, _ in network.items():
         ret[line] = find_possible_switch_stations(lines_per_station, line)
     return ret
@@ -51,13 +53,13 @@ def find_index_in_list(lst, el):
             return i
     return -1
 
-def accumulate_stations(stations, start, dest):
-    start_index = find_index_in_list(stations, start)
-    dest_index = find_index_in_list(stations, dest)
+def accumulate_stations(stations: List[str], start: str, dest: str):
+    start_index: int = find_index_in_list(stations, start)
+    dest_index: int = find_index_in_list(stations, dest)
     assert start_index >= 0
     assert dest_index >= 0
-    ret = []
-    indeces = []
+    ret: List[str] = []
+    indeces: List[int] = []
     if start_index < dest_index:
         indeces = range(start_index, dest_index + 1)
     else:
@@ -68,13 +70,13 @@ def accumulate_stations(stations, start, dest):
 
 class Network:
     def __init__(self, city):
-        self.all_info = read_network(city)
-        self.all_lines = {k:v['all_stations'] for k,v in self.all_info.items()}
-        self.all_switches = {k:v['switches'] for k,v in self.all_info.items()}
-        self.all_stations = find_all_stations(self.all_lines)
-        self.lines_per_station = index_network_by_line(self.all_lines)
-        self.switches_per_line = find_all_switches(self.all_lines, self.lines_per_station)
-        self.all_routes = {}
+        self.all_info: Dict[str, Dict[str, List[str]]] = read_network(city)
+        self.all_lines: Dict[str, List[str]] = {k:v['all_stations'] for k,v in self.all_info.items()}
+        self.all_switches: Dict[str, List[str]] = {k:v['switches'] for k,v in self.all_info.items()}
+        self.all_stations: List[str] = find_all_stations(self.all_lines)
+        self.lines_per_station: Dict[str, List[str]] = index_network_by_line(self.all_lines)
+        self.switches_per_line: Dict[str, List[str]] = find_all_switches(self.all_lines, self.lines_per_station)
+        #self.all_routes = {}
         #for start in self.all_stations:
         #    for dest in self.all_stations:
         #        route = self.find_route(start,dest)
@@ -82,13 +84,13 @@ class Network:
     
 
 
-    def find_route(self, start, dest):
-        route = []
-        start_lines = self.lines_per_station[start]
-        dest_lines = self.lines_per_station[dest]
-        common_lines = set(start_lines).intersection(set(dest_lines))
+    def find_route(self, start: str, dest: str):
+        route: List[str] = []
+        start_lines: List[str] = self.lines_per_station[start]
+        dest_lines: List[str] = self.lines_per_station[dest]
+        common_lines: Set[str] = set(start_lines).intersection(set(dest_lines))
         if len(common_lines) > 0:
-            routes = []
+            routes: List[str] = []
             for common_line in common_lines:
                 routes.append( accumulate_stations(self.all_lines[common_line], start, dest) )
             routes.sort(key=lambda x : len(x))
@@ -96,11 +98,11 @@ class Network:
         
         for start_line in start_lines:
             for dest_line in dest_lines:
-                start_switches = self.switches_per_line[start_line]
-                dest_switches = self.switches_per_line[dest_line]
-                common_switches = set(start_switches).intersection(set(dest_switches))
+                start_switches: List[str] = self.switches_per_line[start_line]
+                dest_switches: List[str] = self.switches_per_line[dest_line]
+                common_switches: Set[str] = set(start_switches).intersection(set(dest_switches))
                 if len(common_switches) > 0:
-                    routes = []
+                    routes: List[str] = []
                     for common_switch in common_switches:
                         routes.append( [self.find_route(start, common_switch), self.find_route(common_switch, dest)] )
                     # erst sortieren wir nach der Länge der  Gesamtstrecke und dann nach der Länge des ersten Abschnitts
@@ -114,39 +116,38 @@ class Network:
         #        dest_switches = self.switches_per_line[dest_line]
 
 class Simulation:
-    def __init__(self, network: Network, nb_subway = 4, nb_sbahn = 8):
-        self.network = network
-        self.time = 0
+    def __init__(self, network: Network, nb_subway: int = 4, nb_sbahn: int = 8):
+        self.network: Network = network
+        self.time: int = 0
         self.read_all_stations()
         self.read_all_lines()
         self.read_trains(nb_subway, nb_sbahn)
 
     def read_all_stations(self):
-        self.all_stations = {}
+        self.all_stations: Dict[str, Station] = {}
         for station in self.network.all_stations:
             self.all_stations[station] = Station(station, self)
 
     def read_all_lines(self):
-        self.all_lines = []
+        self.all_lines: List[Line] = []
         for line, info in self.network.all_info.items():
-            all_station_names = info['all_stations']
-            switch_names = info['switches']
+            all_station_names: List[str] = info['all_stations']
+            switch_names: List[str] = info['switches']
             if '' in switch_names:
                 switch_names.remove('')
-            all_stations = [self.all_stations[station] for station in all_station_names] 
-            switches = [self.all_stations[station] for station in switch_names] 
+            all_stations: List[Station] = [self.all_stations[station] for station in all_station_names] 
+            switches: List[Station] = [self.all_stations[station] for station in switch_names] 
             self.all_lines.append(Line(line, all_stations, switches))
 
     def read_trains(self, nb_subway, nb_sbahn):
-        self.trains = []
-        nb_trains = 0
+        self.trains: List[Train] = []
+        nb_trains: int = 0
         for line in self.all_lines:
-            stations = line.all_stations
-            direction = 1
+            stations: List[Station] = line.all_stations
+            direction: int = 1
+            nb_skip: int = nb_sbahn
             if line.is_subway:
                 nb_skip = nb_subway
-            else:
-                nb_skip = nb_sbahn
             for i in range(0,len(stations), nb_skip):
                 train = Train( line, stations[i], direction, nb_trains)
                 train.update()
@@ -187,7 +188,7 @@ class Simulation:
         print({k:v for k,v in sorted(stations.items(), key=lambda item : item[1], reverse=True)})
 
     def print_lanes(self):
-        for name, station in self.all_stations.items():
+        for _, station in self.all_stations.items():
             print('station ' + station.name + ' has following lanes')
             print(station.lanes)
             print()
@@ -199,33 +200,36 @@ class Simulation:
                 print('    ' + str(subroute))
 
 class Station:
-    def __init__(self, name, sim: Simulation):
-        self.name = name
-        self.sim = sim
-        self.lanes = find_neighbours(self.sim.network, name)
-        self.trains = []
+    def __init__(self, name: str, sim: Simulation):
+        self.name: str = name
+        self.sim: Simulation = sim
+        self.lanes: Dict[str, List[str]] = find_neighbours(self.sim.network, name)
+        self.trains: List[Train] = []
 
     def __str__(self):
         return self.name
 
     def register_arrival(self, train):
-        self.trains.append(train)
+        t: Train = train
+        self.trains.append(t)
 
     def register_departure(self, train):
-        self.trains.remove(train)
+        t: Train = train
+        self.trains.remove(t)
 
     def can_arrive(self, train):
-        relevant_lines = self.lanes[train.current_station.name]
+        tr: Train = train
+        relevant_lines: List[str] = self.lanes[train.current_station.name]
         can_arrive = True
         #reason = ""
         for t in self.trains:
             # Die Zielstation der Blockierenden darf nicht meine aktuelle Station sein.
             #TODO auf Line Objekte umstellen
-            if t.line.name in relevant_lines and t.target_station != train.current_station and t.line.is_subway == train.line.is_subway:
+            if t.line.name in relevant_lines and t.target_station != tr.current_station and t.line.is_subway == tr.line.is_subway:
                 #reason = str(t)
                 can_arrive = False
-        #if train.line:
-        #     print("Can train " + str(train) + " arrive at " + str(self) + "?")
+        #if tr.line:
+        #     print("Can train " + str(tr) + " arrive at " + str(self) + "?")
         #     if can_arrive:
         #         print("    Yes.")
         #     else: 
@@ -233,10 +237,10 @@ class Station:
         return can_arrive
 
 class Route:
-    def __init__(self, from_station, to_station, linename):
-        self.linenname = linename
-        self.from_station = from_station
-        self.to_station = to_station
+    def __init__(self, from_station: Station, to_station: Station, linename: str):
+        self.linename: str = linename
+        self.from_station: Station = from_station
+        self.to_station: Station = to_station
 
     def __eq__(self, other):
         return self.from_station == other.from_station and self.to_station == other.to_station
@@ -245,10 +249,10 @@ class Route:
     def __str__(self):
         return str(self.from_station) + ' -> ' + str(self.to_station)
 
-def find_subline(stations, route: Route, linename):
-    ret = []
-    from_index = find_index_in_list(stations, route.from_station)
-    to_index = find_index_in_list(stations, route.to_station)
+def find_subline(stations: List[Station], route: Route, linename: str):
+    ret: List[Station] = []
+    from_index:int = find_index_in_list(stations, route.from_station)
+    to_index: int = find_index_in_list(stations, route.to_station)
     if from_index < 0 or to_index < 0:
         raise ValueError(str(route) + " not found in line " + linename)
     if from_index < to_index:
@@ -259,14 +263,14 @@ def find_subline(stations, route: Route, linename):
             ret.append(stations[i])
     return ret
 
-def find_sublines(all_stations, routes, linename):
-    sublines = {}
+def find_sublines(all_stations: List[Station], routes: List[Route], linename: str):
+    sublines: Dict[Route, List[Station]] = {}
     for route in routes:
         sublines[route] = find_subline(all_stations, route, linename)
     return sublines
 
-def find_routes(switches, stations, linename):
-    routes = []
+def find_routes(switches: List[Station], stations: List[Station], linename: str):
+    routes: List[Route] = []
     if len(switches) == 0:
         return [Route(stations[0], stations[-1], linename)]
     for begin in switches:
@@ -274,28 +278,28 @@ def find_routes(switches, stations, linename):
             if begin != end:
                 route = Route(begin, end, linename)
                 # nur wenn eine Route durch den Tunnel geht wollen wir von dort nach dort S Bahnen fahren lassen
-                subline = find_subline(stations, route, linename)
-                subline_names = [station.name for station in subline]
+                subline: List[Station] = find_subline(stations, route, linename)
+                subline_names: List[str] = [station.name for station in subline]
                 if "Hauptbahnhof" in subline_names:
                     routes.append(route)
     return routes
 
 class Line:
-    def __init__(self, name, all_stations, switches):
-        self.name = name
-        self.is_subway = self.name.startswith('U')
-        self.all_stations = all_stations
-        self.switches = switches
+    def __init__(self, name: str, all_stations: List[Station], switches: List[Station]):
+        self.name: str = name
+        self.is_subway: bool = self.name.startswith('U')
+        self.all_stations: List[Station] = all_stations
+        self.switches: List[Station] = switches
         #TODO
-        self.routes = find_routes(self.switches, self.all_stations, self.name)
-        self.sublines = find_sublines(self.all_stations, self.routes, self.name)
+        self.routes: List[Route] = find_routes(self.switches, self.all_stations, self.name)
+        self.sublines: Dict[Route, List[Station]] = find_sublines(self.all_stations, self.routes, self.name)
 
     def __str__(self):
         return self.name
 
 
-def find_next_station(current_station, stations, direction):
-    current_index = find_index_in_list(stations, current_station)
+def find_next_station(current_station: Station, stations: List[Station], direction: int):
+    current_index: int = find_index_in_list(stations, current_station)
     current_index += direction
     if current_index < 0:
         direction *= -1
@@ -306,14 +310,14 @@ def find_next_station(current_station, stations, direction):
     return stations[current_index], direction
 
 class Train:
-    def __init__(self, line:Line, starting_station, direction, number):
-        self.number = number
-        self.line = line
-        self.stations = line.all_stations #todo make subline available here
-        self.current_station = starting_station
-        self.target_station = starting_station
-        self.direction = direction
-        self.waiting = False # a train will always wait for one update call before leaving the station
+    def __init__(self, line:Line, starting_station: Station, direction: int, number: int):
+        self.number: int = number
+        self.line: Line = line
+        self.stations: List[Station] = line.all_stations #todo make subline available here
+        self.current_station: Station = starting_station
+        self.target_station: Station = starting_station
+        self.direction: int = direction
+        self.waiting: bool = False # a train will always wait for one update call before leaving the station
         self.delay = defaultdict(int)
 
     def __str__(self):
@@ -339,18 +343,18 @@ class Train:
         else:
             self.arrive()
 
-def find_neighbour(lanes, stations, index, line):
+def find_neighbour(lanes: Dict[str, List[str]], stations: List[str], index, line):
     if index >= 0 and index < len(stations):
-            neighbour_station = stations[index]
-            if neighbour_station in lanes:
-                lanes[neighbour_station].append(line)
-            else:
-                lanes[neighbour_station] = [line]
+        neighbour_station = stations[index]
+        if neighbour_station in lanes:
+            lanes[neighbour_station].append(line)
+        else:
+            lanes[neighbour_station] = [line]
     
-def find_neighbours(network, station):
-    lanes = {}
+def find_neighbours(network: Network, station: str):
+    lanes: Dict[str, List[str]] = {}
     for line, stations in network.all_lines.items():
-        index = find_index_in_list(stations, station)
+        index: int = find_index_in_list(stations, station)
         if index == -1:
             continue
         find_neighbour(lanes, stations, index - 1, line)
