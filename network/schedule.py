b402caa756544b4c57c8883bc79c8f1fdad975ed
mpohl100 <mpohl100@googlemail.com>
2020-08-14 16:08:22 +0200

    Schedule gibt jetzt die Indizes der Start-Stationen aus. TODO Diese Notation muss in Startminuten der Endstationen umgerechnet werden.

--- network/schedule.py
+++ network/schedule.py
@@ -1,6 +1,18 @@
+from network import find_index_in_list
+
 from collections import defaultdict
 from operator import itemgetter
 from typing import List
+
+def get_offsets(nb: int):
+    offsets = []
+    for i in range(0, int(nb / 2 ) + 1):
+        if i == 0:
+            offsets.append(i)
+        else:
+            offsets.append(i)
+            offsets.append(-i)
+    return offsets
 
 class Schedule:
     def __init__(self, lines: List[List[str]]):
@@ -9,17 +21,30 @@ class Schedule:
         for line in self.lines:
             for station in line:
                 self.occurences[station] += 1
-        self.takt = max(self.occurences.items(), key=itemgetter(1))
+        self.main_station_occurence = max(self.occurences.items(), key=itemgetter(1))
 
     def calc(self):
-        
+        main_station = self.main_station_occurence[0]
+        takt = self.main_station_occurence[1]
+        offsets = get_offsets(len(self.lines))
+        for i, line in enumerate(self.lines):
+            main_index = find_index_in_list(line, main_station)
+            offset = offsets[i]
+            print(line[main_index])
+            print(offset)
+            positive_range = list(range(main_index + offset, len(line), +takt))
+            negative_range = list(range(main_index + offset, -len(line), -takt))
+            print(positive_range)
+            print(negative_range)
+
 
 
 if __name__=="__main__":
     from network import Network
     network = [
-        ["Leuchtenbergring", "BergamLaim", "Riem", "Feldkirchen"],
-        ["Leuchtenbergring", "BergamLaim", "Gronsdorf", "Haar"]
+        ["Ostbahnhof","Leuchtenbergring", "BergamLaim", "Riem", "Feldkirchen"],
+        ["Leuchtenbergring", "BergamLaim", "Gronsdorf", "Haar"],
+        ["Ostabhnhof","Leuchtenbergring", "BergamLaim", "Fantasie", "Land", "Trudering"]
     ]
     schedule = Schedule(network)
     schedule.calc()
