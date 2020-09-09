908e66743aa8f1f5f1cb9182a165eae3b4d08819
mpohl100 <mpohl100@googlemail.com>
2020-09-09 17:57:53 +0200

    mit der update Funktion des Passagiers weitergemacht.

--- lib/passenger.py
+++ lib/passenger.py
@@ -9,18 +9,35 @@ class Passenger:
         self.minute = 0
         self.start_minute = start_minute
         self.current_leg = 0
-        self.current_station = self.route[self.current_leg].from_station
+        self.current_station = None
+        self.current_train = None
         self.in_train = False
 
     def update(self):
         self.minute += 1
-        if self.minute < self.start_minute:
+        if self.minute < self.start_minute or self.current_leg == len(self.route):
             return
+        if not self.current_station:
+            self.current_station = self.route[self.current_leg].from_station
+            self.current_station.enter_passenger(self)
         if not self.in_train:
             # nachschauen ob ein Zug mit dem Liniennamen im Bahnhof ist
             linename = self.route[self.current_leg].linename
             train_index = find_index_in_list_pred(self.current_station.trains, lambda x: x.line.name == linename)
-            if train_index != -1:
-                train = self.current_station.trains[train_index]
+            if train_index != -1: #TODO noch überprüfen, dass es die richtige Richtung ist
+                self.current_train = self.current_station.trains[train_index]
                 self.current_station.depart_passenger(self)
-                train.enter_passenger(self)
+                self.current_train.enter_passenger(self)
+        else:
+            target_station = self.route[self.current_leg].to_station
+            if self.current_train.current_station == target_station:
+                self.current_train.depart_passenger(self)
+                target_station.enter_passenger(self)
+                self.current_leg += 1
+                self.in_train = False
+                self.current_station = target_station
+
+
+
+
+
